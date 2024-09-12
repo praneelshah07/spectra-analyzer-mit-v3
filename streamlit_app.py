ceb1dd44c754a99bf152164e66afca3605af50d6
praneelshah07 <praneelshah101@gmail.com>
2024-09-12 09:32:20 -0700

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import linkage
 import requests
 
-# Preloaded zip
+# preloaded zip file
 ZIP_URL = 'https://raw.githubusercontent.com/praneelshah07/MIT-Project/main/ASM_Vapor_Spectra.csv.zip'
 
 def load_data_from_zip(zip_url):
@@ -41,22 +41,22 @@ def load_data_from_zip(zip_url):
         st.error(f"Error extracting CSV from ZIP: {e}")
         return None
 
-# Function to compute the ordered distance matrix
+# ordered distance matrix
 def compute_serial_matrix(dist_mat, method="ward"):
     res_linkage = linkage(dist_mat, method=method)
     res_order = np.array(res_linkage[:, :2], dtype=int).flatten()
     ordered_dist_mat = dist_mat[res_order][:, res_order]
     return ordered_dist_mat, res_order, res_linkage
 
-# Set up app
+# set up app
 st.title("Spectra Visualization App")
 
-# Load data from zip
+# load data from zip
 data = load_data_from_zip(ZIP_URL)
 if data is not None:
     st.write("Using preloaded data from GitHub zip file.")
 
-# File uploader
+# file uploader
 uploaded_file = st.file_uploader("If you would like to enter another dataset, insert it here", type=["csv", "zip"])
 
 if uploaded_file is not None:
@@ -101,14 +101,14 @@ if data is not None:
         if plot_sonogram:
             st.write("The code will take some time to run, please wait...")
 
-            # Filter data based on selected SMILES
+            # filter data based on selected SMILES
             filtered_data = data[data['SMILES'].isin(selected_smiles)]
 
             if not filtered_data.empty:
-                # Create a numpy array of spectra intensities
+                # create an array of spectra intensities
                 intensity_data = np.array(filtered_data['Normalized_Spectra_Intensity'].tolist())
 
-                # If only one molecule is selected, bypass the distance matrix and clustering
+                # if only one molecule is selected, bypass the distance matrix and clustering
                 if len(intensity_data) == 1:
                     fig, ax = plt.subplots(figsize=(12, 12))
                     ax.imshow(intensity_data, aspect='auto', extent=[4000, 500, 1, 0])
@@ -116,11 +116,11 @@ if data is not None:
                     ax.set_ylabel("Molecule")
                     st.pyplot(fig)
                 else:
-                    # Compute the distance matrix and serial matrix
+                    # compute the distance matrix and serial matrix
                     dist_mat = squareform(pdist(intensity_data))
                     ordered_dist_mat, res_order, res_linkage = compute_serial_matrix(dist_mat, "ward")
 
-                    # Plot the sonogram for multiple molecules
+                    # plot the sonogram for multiple molecules
                     fig, ax = plt.subplots(figsize=(12, 12))
                     ratio = int(len(intensity_data[0]) / len(intensity_data))
                     ax.imshow(np.array(intensity_data)[res_order], aspect=ratio, extent=[4000, 500, len(ordered_dist_mat), 0])
@@ -129,7 +129,6 @@ if data is not None:
 
                     st.pyplot(fig)
 
-             # Add a download button for the sonogram/heatmap
                 buf = io.BytesIO()
                 fig.savefig(buf, format='png')
                 buf.seek(0)
@@ -167,14 +166,14 @@ if data is not None:
                         ax.text(peak_wavelength, peak_intensity + 0.05, f'{round(peak_wavelength, 1)}', 
                                 fontsize=10, ha='center', color=color_palette[i % len(color_palette)])
 
-            # Customize plot
+            # customize plot
             ax.set_xscale('log')
             ax.set_xlim([2.5, 20])
 
             major_ticks = [3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 20]
             ax.set_xticks(major_ticks)
 
-            # Number of label matches number of ticks
+            # num of label matches number of ticks
             ax.set_xticklabels([str(tick) for tick in major_ticks])
 
             ax.tick_params(direction="in",
